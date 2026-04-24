d9592b185918a699ec1b7c855e9363305e2009e2
TM-Infra-AI <infra@technologymindz.com>
2026-04-24 15:24:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
  
  <meta name="description" content="Best digital visiting card online with many designs, Now create in just 5 minutes and get it instantly.">
   <!-- Required meta tags -->
+	<!-- Required meta tags -->
   <meta charset="utf-8" />
   <link rel='stylesheet' href='panel/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
   <meta name="google-site-verification" content="RjyVv4ISQhKLzqEfKPe3fFENreEjM1lMHeCE5r99hMs" />
